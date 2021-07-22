d907cb791b8da4c0e108c8f6695bef363291920d
Jahaziel0311 <62921040+Jahaziel0311@users.noreply.github.com>
2021-07-21 22:32:49 -0500

    Carga inicial

---
+++ src/test/java/UTest/runners/RunnerTags.java
@@ -0,0 +1,16 @@
+package UTest.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features="src/test/resources/features/UTest.feature",
+        tags="@stories",
+        glue="UTest.stepdefinitions",
+        snippets= SnippetType.CAMELCASE)
+
+public class RunnerTags {
+}
